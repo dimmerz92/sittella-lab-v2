8d3d384625e96552f86a83b8224ed1bf6a5d66c1
Andrew <andrew.weymes@outlook.com>
2024-03-18 11:02:10 +0800

    init go echo project

---
+++ cmd/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(".env not found")
+	}
+
+	port, exists := os.LookupEnv("PORT")
+	if !exists {
+		port = "8000"
+	}
+
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(":" + port))
+}
